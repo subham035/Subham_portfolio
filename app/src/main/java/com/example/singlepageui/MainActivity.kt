3597a159a9028cfb66b32ee787320d05bc19cd14
subham035 <subhammajhi669@gmail.com>
2024-06-02 11:07:16 +0530

    final commit

---
+++ app/src/main/java/com/example/singlepageui/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.singlepageui
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun openGithub(view: View) {
+        openUrl("https://subhammajhi@gmail.com/")
+    }
+    fun openInstagram(view: View) {
+        openUrl("https://www.instagram.com/")
+    }
+    fun openLinkdin(view: View) {
+        openUrl("https://www.linkedin.com/in/subham-satyasundar-majhi-ab6424303/")
+    }
+    fun openGmail(view: View) {
+        openUrl("https://github.com/subham035/")
+    }
+
+
+    private fun openUrl(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        startActivity(intent)}
+
+
+}
